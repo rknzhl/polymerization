7c090d231e60aebbb9def8d3efb937bc0453418f
Rodion Akinzhala <roakin39@yandex.ru>
2024-06-04 14:52:59 +0300

    new era

--- Protein.py
+++ Protein.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import imageio.v3 as iio
+from datetime import datetime
+import os
+
 
 class ProteinModel:
     def __init__(self, len_beads, temperature=300):
@@ -9,8 +13,8 @@ class ProteinModel:
         self.temperature = temperature
         self.kB = 1.38e-23
         self.positions = np.zeros((self.num_beads, 3))
-        self.spring_constant = 1.0  # примерно значение пружинной константы
-        self.bond_length = 1.0  # примерное значение длины связи
+        self.spring_constant = 1.0
+        self.bond_length = 1.0
 
         index = 0
         for x in range(len_beads):
@@ -20,7 +24,6 @@ class ProteinModel:
                     index += 1
 
     def energy(self):
-        """ Рассчитать энергию системы на основе текущих позиций """
         energy = 0.0
         for i in range(1, self.num_beads):
             displacement = self.positions[i] - self.positions[i - 1]
@@ -29,10 +32,9 @@ class ProteinModel:
         return energy
 
     def monte_carlo_step(self):
-        """ Выполнить один шаг Монте-Карло """
         for i in range(self.num_beads):
             old_position = np.copy(self.positions[i])
-            random_displacement = np.random.normal(0, 0.1, 3)  # случайное смещение
+            random_displacement = np.random.normal(0, 0.1, 3)
             self.positions[i] += random_displacement
             old_energy = self.energy()
             new_energy = self.energy()
@@ -40,23 +42,53 @@ class ProteinModel:
             if delta_e > 0:
                 probability = np.exp(-delta_e / (self.kB * self.temperature))
                 if np.random.rand() > probability:
-                    self.positions[i] = old_position  # отклонить изменение
+                    self.positions[i] = old_position
 
     def run_simulation(self, num_steps=100):
-        """ Запустить симуляцию """
+        frames = []
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
+
+        ax.set_xlim([0, self.len_beads + 1])
+        ax.set_ylim([0, self.len_beads + 1])
+        ax.set_zlim([0, self.len_beads + 1])
+
+        ax.xaxis.pane.fill = False
+        ax.yaxis.pane.fill = False
+        ax.zaxis.pane.fill = False
+
+        ax.xaxis.pane.set_edgecolor('w')
+        ax.yaxis.pane.set_edgecolor('w')
+        ax.zaxis.pane.set_edgecolor('w')
+        print(f'Начало симуляции с количеством шагов: {num_steps}')
         for step in range(num_steps):
             self.monte_carlo_step()
             ax.clear()
-            ax.scatter(self.positions[:, 0], self.positions[:, 1], self.positions[:, 2])
-            plt.pause(0.05)  # Пауза для отображения gi
-        plt.show()
+            ax.scatter(self.positions[:, 0], self.positions[:, 1], self.positions[:, 2], c='blue', s=100)
+            plt.title(f'Step {step + 1}')
+
+            ax.set_xlim([0, self.len_beads])
+            ax.set_ylim([0, self.len_beads])
+            ax.set_zlim([0, self.len_beads])
+
+            plt.draw()
+            fig.canvas.draw()
+
+            image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+            image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+            frames.append(image)
+
+        plt.close()
+
+        # Создание папки, если она не существует
+        if not os.path.exists('videos'):
+            os.makedirs('videos')
+
+        # Сохранение в видео
+        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        video_path = f'videos/simulation_{current_time}.mp4'
+        iio.imsave(video_path, frames, fps=10)
+        print(f'Видео сохранено в {video_path}')
 
     def get_positions(self):
-        """ Получить текущие позиции """
         return self.positions
-
-# Использование класса
-model = ProteinModel(len_beads=4)
-model.run_simulation(num_steps=1)
